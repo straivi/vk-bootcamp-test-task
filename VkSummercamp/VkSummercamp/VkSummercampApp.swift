184fc2cc8391da53df9e57e47b191eea31d97d18
Matvei Borisov <bme15122000@gmail.com>
2022-07-12 20:49:46 +0500

    init swift ui app

---
+++ VkSummercamp/VkSummercamp/VkSummercampApp.swift
@@ -0,0 +1,17 @@
+//
+//  VkSummercampApp.swift
+//  VkSummercamp
+//
+//  Created by Матвей Борисов on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct VkSummercampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
