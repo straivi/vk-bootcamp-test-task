95d51deee604268862c8c083e82fab68c3558ab7
Matvei Borisov <bme15122000@gmail.com>
2022-07-13 18:05:34 +0500

    create entity with colors

---
+++ VkSummercamp/VkSummercamp/Helpers/AppColors.swift
@@ -0,0 +1,13 @@
+//
+//  AppColors.swift
+//  VkSummercamp
+//
+//  Created by Матвей Борисов on 13.07.2022.
+//
+
+import SwiftUI
+
+struct AppColors {
+    static let vkBlue = Color(red: 0/255, green: 119/255, blue: 255/255)
+    static let vkMagenta = Color(red: 255/255, green: 0/255, blue: 132/255)
+}
